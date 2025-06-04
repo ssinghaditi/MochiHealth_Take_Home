d059d4b2640cc6f049fe434d30d17e42b1fc3822
Aditi Singh <ssingh.adii@gmail.com>
2025-06-03 23:55:39 -0400

    Updating google credentials 2

--- mochi_health.py
+++ mochi_health.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # Google Sheets connection
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds_dict = st.secrets["gcp_service_account"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(creds_dict, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("Mochi Health").sheet1
